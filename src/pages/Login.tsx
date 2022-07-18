141baaf7d4aece498d304860ebe886f6b83b6424
Mateus Capitanio <mateusjmj92@gmail.com>
2022-07-18 12:24:12 -0300

    Tela de login - [Finalizada]

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,49 @@
-import React, { useContext } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// import { IChangeButton } from "../interfaces/ChangeButton";
 // import myContext from '../contexts/myContext';
 
 // CSS
 import "../styles/loginStyle.css";
 
 function Login() {
-  // const context = useContext(myContext)
+  const navigate = useNavigate();
+
+  const [validateEmail, setValidadeEmail] = useState(false);
+  const [validateSenha, setValidadeSenha] = useState(false);
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const valueReturn = localStorage.getItem("user");
+    // const user = JSON.parse(valueReturn);
+    if (valueReturn !== null) {
+      navigate("/home");
+    }
+  }, []);
+
+  const checkInput = (event?: any): void => {
+    const emailRegex = /^\w+@[a-z]+(\.[a-z]+){1,2}$/;
+    const { target } = event;
+
+    if (target.id === "email") {
+      const validate = emailRegex.test(target.value);
+      setValidadeEmail(validate);
+      setEmail(target.value);
+    }
+    if (target.id === "senha") {
+      const validate = target.value.length >= 8;
+      setValidadeSenha(validate);
+    }
+  };
+
+  const btnSubmit = (e: any) => {
+    e.preventDefault();
+
+    const emailString = JSON.stringify(email);
+    localStorage.setItem("user", emailString);
+    navigate("/home");
+  };
 
   return (
     <div>
@@ -13,9 +51,28 @@ function Login() {
         <section className="loginSection">
           <h1>Login</h1>
           <form>
-            <input placeholder="Email..." type="email" />
-            <input placeholder="Senha..." type="password" />
-            <button type="submit">Entrar</button>
+            <input
+              id="email"
+              required
+              onChange={checkInput}
+              placeholder="Email..."
+              type="email"
+            />
+            <input
+              id="senha"
+              required
+              onChange={checkInput}
+              placeholder="Senha..."
+              type="password"
+            />
+            <button
+              id="buttonSubmit"
+              onClick={btnSubmit}
+              type="submit"
+              disabled={!(validateEmail && validateSenha)}
+            >
+              Entrar
+            </button>
           </form>
         </section>
       </main>
