23c9de67d8c50b83d5a8e7b849e4de6e3db325a7
Mateus Capitanio <mateusjmj92@gmail.com>
2022-07-20 20:58:05 -0300

    Consertando bug da tela de login

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -5,7 +5,7 @@ import "../styles/notFoundStyle.css";
 function Notfound() {
   return (
     <div>
-      <main>
+      <main className="mainNotFound">
         <div className="divText">
           <h1 className="ops">OOPS...</h1>
           <h1 className="text">NÃO HÁ NADA POR AQUI!</h1>
